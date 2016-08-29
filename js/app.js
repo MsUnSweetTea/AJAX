8968cea1fcf35ce094e9f49b4d9c6c84d86269be
LavieMaru <laviemaru@gmail.com>
2016-08-29 12:42:56 -0400

    commit 2

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
 
 $(function(){
 
-  $.get('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    console.log(data);
-  }, 'json');
-})
+  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    var myData = data.Search;
 
-
+    $.each(myData, function(index, value){ //index represents the element being looped, and value represents the property
+    	console.log(value.Title);
+    })
+    console.log(myData);
+    // console.log(myData[9]['Title']);
+});
 });
